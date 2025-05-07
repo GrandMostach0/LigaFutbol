abd11b711810378570ded60bcbbd198a5a4fa52e
GrandMostach0 <kreedlegend0@gmail.com>
2025-05-07 12:15:28 -0600

    anexo del banner inicial

--- src/page/Main.jsx
+++ src/page/Main.jsx
@@ -1,11 +1,24 @@
 import React from "react";
+import {useNavigate } from "react-router-dom";
+import "../App.css";
 
 function Main(){
 
-    return(
-        <h1>HOLA SOY DEL MAIN</h1>
-    )
+    const navigate = useNavigate();
 
+    return(
+        <section className="py-35">
+            <div className="m-auto w-2xl text-center">
+                <h1 className="font-bold text-5xl mb-2">Liga de Futbol 2025</h1>
+                <p>Toda la emoción del fútbol en un solo lugar. Sigue a tu equipo favorito en todas las categorías.</p>
+                <br />
+                <button className="btnPrimary text-white bg-black">Ver partidos</button>
+                <button className="btnPrimary ">Clasificación</button>
+            </div>
+        </section>
+
+        
+    )
 }
 
 export default Main;
